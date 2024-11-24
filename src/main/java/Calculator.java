6d5075e679aa96e03ec107f6d0b7d6d396a0f3f4
KonovalovAles <darkelf1979@gmail.com>
2024-11-24 12:27:00 +0300

    Проверяйте.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         String inputStr = scanner.nextLine();
 
 
-        System.out.println("" + calc(inputStr));
+        System.out.println(calc(inputStr));
     }
 
 
@@ -15,28 +15,53 @@ public class Calculator {
         String[] args = input.split("\\s+");
         String regexAct = "[+-*/]";
         String regexNumeric = "[^0-9]";
-        int result;
-        int arg1;
-        int arg2;
+        String funcRes;
 
+        int result = 0;
 
-        private int add(int arg1, int arg2){
-            return arg1 + arg2;
+        //Проверка входных данных
+        //кол-во операторов
+        try{
+            if (args.length != 3) throw new RuntimeException();
+
+        }catch (Exception ex){
+            System.out.println("Неверная запись строки");
+            System.out.println(" операнд оператор операнд");
+            throw ex;
         }
 
-        private  int multi(int arg1, int arg2){
-            return arg1 * arg2;
+        //проверка данных на цифры и допустимые операторы
+        try{
+            if (!args[1].matches(regexAct)){
+                throw new RuntimeException();
+            }
+
+            if(!args[0].matches(regexNumeric) || !args[2].matches(regexNumeric)){
+                throw new RuntimeException();
+            }
+
+        }catch (Exception ex){
+            System.out.println("Неверные входнные данные");
+            System.out.println("Допустимы 0-9 и */-+");
+            throw ex;
         }
 
-        private int minus(int arg1, int arg2){
-            return arg1 - arg2;
+        int arg1 = Integer.parseInt(args[0]);
+        int arg2 = Integer.parseInt(args[2]);
+
+        if(args[1].equals("+")){
+            result =  arg1 + arg2;
+        } else if (args[1].equals("-")){
+            result =  arg1 - arg2;
+        } else if (args[1].equals("*")){
+            result =  arg1 * arg2;
+        } else if(args[1].equals("/")){
+            result =  arg1 / arg2;
         }
 
-        private int divison(int arg1, int arg2){
-            return arg1 % arg2;
-        }
+        funcRes ="Результат операции: " + result;
 
-        return result;
+        return funcRes;
     }
 
 }

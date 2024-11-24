11654526a3f3c40990847e1c8411cef25f27cfcb
KonovalovAles <darkelf1979@gmail.com>
2024-11-24 16:46:36 +0300

    Добавил проверту операндов от 0 до 10.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,67 +1,77 @@
 import java.util.Scanner;
 
 public class Calculator {
+    public static final String REGEX_ACT = "[+-*/]";
+    public static final String REGEX_NUMERIC = "[^0-9]";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputStr = scanner.nextLine();
-
+        scanner.close();
 
         System.out.println(calc(inputStr));
     }
 
 
-    public static String calc(String input){
+    public static String calc(String input) {
 
         String[] args = input.split("\\s+");
-        String regexAct = "[+-*/]";
-        String regexNumeric = "[^0-9]";
-        String funcRes;
 
         int result = 0;
 
         //Проверка входных данных
         //кол-во операторов
-        try{
+        try {
             if (args.length != 3) throw new RuntimeException();
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("Неверная запись строки");
             System.out.println(" операнд оператор операнд");
             throw ex;
         }
 
         //проверка данных на цифры и допустимые операторы
-        try{
-            if (!args[1].matches(regexAct)){
+        try {
+            if (!args[1].matches(REGEX_ACT)) {
                 throw new RuntimeException();
             }
 
-            if(!args[0].matches(regexNumeric) || !args[2].matches(regexNumeric)){
+            if (!args[0].matches(REGEX_NUMERIC) || !args[2].matches(REGEX_NUMERIC)) {
                 throw new RuntimeException();
             }
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("Неверные входнные данные");
             System.out.println("Допустимы 0-9 и */-+");
             throw ex;
         }
 
+
         int arg1 = Integer.parseInt(args[0]);
         int arg2 = Integer.parseInt(args[2]);
 
-        if(args[1].equals("+")){
-            result =  arg1 + arg2;
-        } else if (args[1].equals("-")){
-            result =  arg1 - arg2;
-        } else if (args[1].equals("*")){
-            result =  arg1 * arg2;
-        } else if(args[1].equals("/")){
-            result =  arg1 / arg2;
+        try {
+            if (arg1 > 10 || arg1 < 0 || arg2 < 0 || arg2 > 10){
+                throw new RuntimeException();
+            }
+        } catch (Exception ex){
+            System.out.println("Неверные входнные данные");
+            System.out.println("Допустимые числа 0 - 10");
+            throw ex;
         }
 
-        funcRes ="Результат операции: " + result;
 
-        return funcRes;
+        if (args[1].equals("+")) {
+            result = arg1 + arg2;
+        } else if (args[1].equals("-")) {
+            result = arg1 - arg2;
+        } else if (args[1].equals("*")) {
+            result = arg1 * arg2;
+        } else if (args[1].equals("/")) {
+            result = arg1 / arg2;
+        }
+
+        return "Результат операции: " + result;
     }
 
 }
